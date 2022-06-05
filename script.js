33214d07c1764e6bd31c8f64fccce641d66d5689
Himalaya <yugmani@yahoo.com>
2022-06-05 11:42:50 -0700

    html structure and basic styling

---
+++ script.js
@@ -0,0 +1,6 @@
+const billTotal = document.getElementById("bill-amount");
+
+const tipTotal = document.getElementById("tip-amount");
+const grandTotal = document.getElementById("grand");
+
+console.log("bill total: ", billTotal.value);
